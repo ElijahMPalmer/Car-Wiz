83c558ba060e5c5e8aa057182fb5bdf7604b1c0f
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-03-01 14:51:27 -0800

    Implemented an array of objects to hold all of the vehicle makes, and resources websites. Making the website easier to edit and add more vehicle makes in the future. This also improves runtime without the use of a Switch statement, only one if/ else.

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ new fullpage('#fullpage', {
     anchors: ['section1', 'section2']
 })
 
+const carMakes = [
+    { make: 'acura', manual: 'https://owners.acura.com/vehicles/warranty', forum: 'https://acurazine.com/forums/' },
+    { make: 'kia', manual: 'https://owners.kia.com/content/owners/en/manuals.html', forum: 'https://www.kia-forums.com/' },
+    { make: 'ford', manual: 'https://www.ford.com/support/owner-manuals/', forum: 'https://www.fordforums.com/' },
+    { make: 'chevrolet', manual: 'https://www.chevrolet.com/support/vehicle/manuals-guides', forum: 'https://chevroletforum.com/forum/' },
+    { make: 'honda', manual: 'https://owners.honda.com/vehicle-information/manuals', forum: 'https://honda-tech.com/forums/' },
+    { make: 'toyota', manual: 'https://www.toyota.com/owners/resources/warranty-owners-manuals', forum: 'https://www.toyotanation.com/' },
+    { make: 'nissan', manual: 'https://www.nissanusa.com/owners/ownership/manuals-guides.html', forum: 'https://forums.nicoclub.com/' },
+    { make: 'jeep', manual: 'https://www.mopar.com/en-us/my-vehicle/vehicle-information.html?redirect=om', forum: 'https://www.jeepforum.com/forums/' },
+    { make: 'hyundai', manual: 'https://owners.hyundaiusa.com/us/en/resources/manuals-warranties.html', forum: 'https://www.hyundai-forums.com/forums/' },
+    { make: 'dodge', manual: 'https://www.mopar.com/en-us/my-vehicle/vehicle-information.html?redirect=om', forum: 'https://www.dodgetalk.com/forums/' },
+    { make: 'subaru', manual: 'https://www.subaru.com/owners/vehicle-resources.html', forum: 'https://www.thesubaruforums.com/' },
+    { make: 'gmc', manual: 'https://www.gmc.com/support/vehicle/manuals-guides', forum: 'https://www.gmtruckclub.com/' },
+    { make: 'volkswagen', manual: 'https://www.vwserviceandparts.com/digital-resources/online-owners-manual/', forum: 'https://www.vwforum.com/' },
+    { make: 'bmw', manual: 'https://www.bmwusa.com/owners-manuals.html', forum: 'https://www.bimmerpost.com/' },
+    { make: 'mercedes benz', manual: 'https://www.mbusa.com/en/owners/manuals', forum: 'https://forums.mercedesclub.org.uk/index.php' },
+    { make: 'tesla', manual: 'https://www.tesla.com/ownersmanual', forum: 'https://teslamotorsclub.com/tmc/' },
+];
+
 const $vin = $('#entry');
 const $submit = $('#submit');
 $submit.click(function() {
@@ -32,126 +51,22 @@ $submit.click(function() {
             $vinEngineHP.appendTo($vehInfo)
 
             let make = (data.Results[0].Make).toLowerCase();
+            let foundMake = carMakes.find(car => car.make === make);
             let $ownersMan;
             let $forum;
             let $noRes;
             const $resources = $('<h2>Resources</h2>')
-            switch (make) {
-                case 'acura':
-                    $ownersMan = $('<h3><a href="https://owners.acura.com/vehicles/warranty" target="_blank" > Owners Manuals </a></h3 > ');
-                    $forum = $('<h3><a href="https://acurazine.com/forums/" target="_blank">Forums</a></h3>');
 
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'kia':
-                    $ownersMan = $('<h3><a href="https://owners.kia.com/content/owners/en/manuals.html" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://www.kia-forums.com/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'ford':
-                    $ownersMan = $('<h3><a href="https://www.ford.com/support/owner-manuals/" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://www.fordforums.com/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'chevrolet':
-                    $ownersMan = $('<h3><a href="https://www.chevrolet.com/support/vehicle/manuals-guides" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://chevroletforum.com/forum/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'honda':
-                    $ownersMan = $('<h3><a href="https://owners.honda.com/vehicle-information/manuals" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://honda-tech.com/forums/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'toyota':
-                    $ownersMan = $('<h3><a href="https://www.toyota.com/owners/resources/warranty-owners-manuals" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://www.toyotanation.com/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'nissan':
-                    $ownersMan = $('<h3><a href="https://www.nissanusa.com/owners/ownership/manuals-guides.html" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://forums.nicoclub.com/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'jeep':
-                    $ownersMan = $('<h3><a href="https://www.mopar.com/en-us/my-vehicle/vehicle-information.html?redirect=om" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://www.jeepforum.com/forums/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'hyundai':
-                    $ownersMan = $('<h3><a href="https://owners.hyundaiusa.com/us/en/resources/manuals-warranties.html" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://www.hyundai-forums.com/forums/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'dodge':
-                    $ownersMan = $('<h3><a href="https://www.mopar.com/en-us/my-vehicle/vehicle-information.html?redirect=om" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://www.dodgetalk.com/forums/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'subaru':
-                    $ownersMan = $('<h3><a href="https://www.subaru.com/owners/vehicle-resources.html" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://www.thesubaruforums.com/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'gmc':
-                    $ownersMan = $('<h3><a href="https://www.gmc.com/support/vehicle/manuals-guides" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://www.gmtruckclub.com/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'volkswagen':
-                    $ownersMan = $('<h3><a href="https://www.vwserviceandparts.com/digital-resources/online-owners-manual/" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://www.vwforum.com/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'bmw':
-                    $ownersMan = $('<h3><a href="https://www.bmwusa.com/owners-manuals.html" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://www.bimmerpost.com/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'mercedes benz':
-                    $ownersMan = $('<h3><a href="https://www.mbusa.com/en/owners/manuals" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://forums.mercedesclub.org.uk/index.php" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                case 'tesla':
-                    $ownersMan = $('<h3><a href="https://www.tesla.com/ownersmanual" target="_blank">Owners Manuals</a></h3>');
-                    $forum = $('<h3><a href="https://teslamotorsclub.com/tmc/" target="_blank">Forums</a></h3>');
-                    $resources.appendTo($resource);
-                    $ownersMan.appendTo($resource);
-                    $forum.appendTo($resource);
-                    break;
-                default:
-                    $noRes = $('<h3>No Results for now! Manufacturer will be added soon!</h3>')
+            if (foundMake) {
+                console.log(foundMake, " found make")
+                $ownersMan = $('<h3><a href="' + foundMake.manual + '" target="_blank">Owners Manuals</a></h3>');
+                $forum = $('<h3><a href="' + foundMake.forum + '" target="_blank">Forums</a></h3>');
+                $resources.appendTo($resource);
+                $ownersMan.appendTo($resource);
+                $forum.appendTo($resource);
+            } else {
+                $noRes = $('<h3>No Results for now! Manufacturer will be added soon!</h3>')
+                $noRes.appendTo($resource);
             }
         })
     }
